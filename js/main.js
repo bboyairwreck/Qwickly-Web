c9436f57d244fa1332b206584d402fe5dd005dcc
Eric Chee <bboyairwreck@gmail.com>
2016-03-03 01:33:14 -0800

    Added Reset/Play button to listen for winner of first press

--- js/main.js
+++ js/main.js
@@ -1,32 +1,56 @@
 var qFirebase = new Firebase("https://qwickly.firebaseio.com/");
+var gameFirebase = qFirebase.child("games").child("FOOD");
+var playersFirebase = gameFirebase.child("players");
+
+var isPlaying = false;
 
 $(document).ready(function() {
 	console.log("running");
 
-	var gameFirebase = qFirebase.child("games").child("FOOD");
-	var playersFirebase = gameFirebase.child("players");
-
 	gameFirebase.child("round1").remove();
 
-	gameFirebase.child("round1").limitToLast(1).on("child_added", function(snapsnot, prevChildKey) {
+	listenForWinner();
+
+	gameFirebase.child("round1").on("child_added", function(snapsnot, prevChildKey) {
 		console.log("child_added : " + snapsnot.val());
 		var playerID = snapsnot.val();
 
 		playersFirebase.child(playerID).once("value", function(data) {
 			var playerName = data.val();
 			console.log("playerName : " + playerName);
+
+			if (isPlaying) {
+				console.log("found winner");
+				isPlaying = false;
+				showWinner(playerName);
+			}
+
 			hitList(playerName);
 		});
-		
-		
 	});
 
-	function hitList(name) {
-		$bamList = $("#bamList");
-
-		$li = $("<li>");
-		$li.text(name);
-
-		$bamList.append($li);
-	}
 });
+
+
+function hitList(name) {
+	$bamList = $("#bamList");
+
+	$li = $("<li>");
+	$li.text(name);
+
+	$bamList.append($li);
+}
+
+function listenForWinner() {
+	$("#btnListen").click(function(event) {
+		isPlaying = true;
+		$("#winner").text("");
+		gameFirebase.child("round1").remove();
+		$bamList = $("#bamList").html("");
+		$(this).text("Reset");
+	});
+}
+
+function showWinner(displayName) {
+	$("#winner").text(displayName + " is the winner!!");
+}
